e04f6179836bbea2c4a08ec194d5bea56629b399
Logan <thecrazykid2121@gmail.com>
2024-06-11 00:33:42 -0400

    Skeleton UI
    Erm.... What!

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,21 @@
 <script>
-import "../app.pcss";
-import SideBar from "$lib/components/SideBar.svelte";
+// @ts-nocheck
+
+  import "../app.pcss";
+  import NowPlaying from "$lib/components/NowPlaying.svelte";
+  import TopBar from "$lib/components/TopBar.svelte";
 </script>
 
-<SideBar />
-<slot></slot>
+<div class="flex flex-col h-screen">
+    <div class="border border-gray-300">
+        <TopBar />
+    </div>
+  <div class="flex flex-1 mt-1/10">
+    <div class="w-3/5 border border-gray-300">
+      <slot />
+    </div>
+    <div class="border border-gray-300 w-2/5">
+        <NowPlaying />
+    </div>
+  </div>
+</div>
