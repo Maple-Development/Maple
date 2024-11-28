4aae7d538274153f24f430af32235ec9da1a999b
Cattn <Cattn@mathstudy.dev>
2024-11-28 15:55:00 -0500

    List View Albums/Album

--- src/routes/album/+page.svelte
+++ src/routes/album/+page.svelte
@@ -3,7 +3,7 @@
     import { onMount } from "svelte";
     import type { Album } from "$lib/types/album";
     import type { Song } from "$lib/types/song";
-    import { ArrowUpAZ, ArrowDownZA, ListFilter, SeparatorVertical } from "lucide-svelte";
+    import { ArrowUpAZ, ArrowDownZA, ListFilter, SeparatorVertical, List } from "lucide-svelte";
     import Button from "$lib/components/ui/button/button.svelte";
     import * as DropdownMenu from "$lib/components/ui/dropdown-menu/index.js";
     import { Separator } from "$lib/components/ui/separator/index.js";
@@ -17,7 +17,7 @@
     let tracks: Song[] = [];
     let disks: number = 0;
     let alldisks: Song[][] = [];
-    let listType = "list";
+    let listType = "grid";
 
     onMount(async () => {
         let params = new URLSearchParams(document.location.search);
@@ -96,6 +96,21 @@
         ascending = !ascending;
         orderTracks(sort);
     }
+
+    function formatDuration(duration: number): string {
+      const roundedDuration = Math.round(duration);
+      const minutes = Math.floor(roundedDuration / 60);
+      const seconds = roundedDuration % 60;
+      return `${minutes}:${seconds.toString().padStart(2, '0')}`;
+    }
+
+    function swapListType() {
+        if (listType === "list") {
+            listType = "grid";
+        } else {
+            listType = "list";
+        }
+    }
 </script>
 
 <div class="w-full mt-4 h-10 px-10 flex justify-end">
@@ -124,18 +139,44 @@
                 </DropdownMenu.RadioGroup>
             </DropdownMenu.Content>
         </DropdownMenu.Root>
+        <Button class="my-1 ml-3 h-10 w-10 bg-transparent px-1 hover:bg-secondary" on:click={() => swapListType()}>
+            <List size={20} color="white" />
+        </Button>
 </div>
 
+
+{#if listType !== "grid"}
 <div class="flex flex-col gap-y-8 ml-16 my-5">
-    {#if alldisks.length > 1 && sort === "track"}
-        {#each alldisks as disk, diskIndex}
-            {#if diskIndex > 0}
-                <div class="flex flex-col items-start">
-                    <Separator class="w-[95%] mb-4 mt-1 pr-20"></Separator>
+        {#if alldisks.length > 1 && sort === "track"}
+            {#each alldisks as disk, diskIndex}
+                {#if diskIndex > 0}
+                    <div class="flex flex-col items-start">
+                        <Separator class="w-[95%] mb-4 mt-1 pr-20"></Separator>
+                    </div>
+                {/if}
+                <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-x-4 gap-y-6 sm:gap-x-6 md:gap-x-8 lg:gap-x-10 xl:gap-x-12">
+                    {#each disk as track}
+                        <div class="flex flex-col items-start">
+                            {#await getImageUrl(track.image) then image}
+                                <TrackWrapper track={track} tracks={tracks}>
+                                    <Lazy keep={true}>
+                                        <img class="h-52 w-52 rounded-sm" src={image} alt={track.title} />
+                                    </Lazy>
+                                </TrackWrapper>
+                                <div class="flex flex-col items-start mt-4">
+                                    <h3 class="text-foreground text-lg font-bold leading-none mb-1">{track.title}</h3>
+                                    <p class="text-slate-400 text-base font-light leading-none">{track.artist}</p>
+                                </div>
+                            {:catch error}
+                                <div class="h-52 w-52 bg-gray-500 rounded-sm animate-pulse"></div>
+                            {/await}
+                        </div>
+                    {/each}
                 </div>
-            {/if}
+            {/each}
+        {:else}
             <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-x-4 gap-y-6 sm:gap-x-6 md:gap-x-8 lg:gap-x-10 xl:gap-x-12">
-                {#each disk as track}
+                {#each tracks as track}
                     <div class="flex flex-col items-start">
                         {#await getImageUrl(track.image) then image}
                             <TrackWrapper track={track} tracks={tracks}>
@@ -153,27 +194,65 @@
                     </div>
                 {/each}
             </div>
-        {/each}
+        {/if}
+        </div>
     {:else}
-        <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-x-4 gap-y-6 sm:gap-x-6 md:gap-x-8 lg:gap-x-10 xl:gap-x-12">
-            {#each tracks as track}
-                <div class="flex flex-col items-start">
-                    {#await getImageUrl(track.image) then image}
+        <div class="flex flex-col mx-4 mb-5 mt-2">
+        {#if alldisks.length > 1 && sort === "track"}
+            {#each alldisks as disk, diskIndex}
+                {#if diskIndex > 0}
+                    <div class="flex flex-col items-start">
+                        <Separator class="w-[95%] mb-4 mt-1 pr-20"></Separator>
+                    </div>
+                {/if}
+                <div class="flex flex-col mx-4 mb-5 mt-2">
+                    {#each disk as track}
                         <TrackWrapper track={track} tracks={tracks}>
-                            <Lazy keep={true}>
-                                <img class="h-52 w-52 rounded-sm" src={image} alt={track.title} />
-                            </Lazy>
+                            <div class="flex flex-row items-center hover:bg-secondary py-2 px-2 rounded-sm">
+                                {#await getImageUrl(track.image) then image}
+                                    <Lazy keep={true}>
+                                        <img class="h-24 w-24 mr-4" src={image} alt={track.title} />
+                                    </Lazy>
+                                {:catch error}
+                                    <div class="h-24 w-24 bg-gray-500 mr-4"></div>
+                                {/await}
+                                <div class="flex flex-col items-start flex-grow">
+                                    <h1 class="text-foreground text-lg font-bold leading-none mb-1">{track.title}</h1>
+                                    <h1 class="text-slate-400 text-base font-light leading-none">{track.artist}</h1>
+                                </div>
+                                <div class="flex flex-col items-end text-right ml-4">
+                                    <h1 class="text-slate-400 text-base font-light leading-none">{track.year}</h1>
+                                    <h1 class="text-slate-400 text-base font-light leading-none">{track.artist}</h1>
+                                </div>
+                            </div>
                         </TrackWrapper>
-                        <div class="flex flex-col items-start mt-4">
-                            <h3 class="text-foreground text-lg font-bold leading-none mb-1">{track.title}</h3>
-                            <p class="text-slate-400 text-base font-light leading-none">{track.artist}</p>
-                        </div>
-                    {:catch error}
-                        <div class="h-52 w-52 bg-gray-500 rounded-sm animate-pulse"></div>
-                    {/await}
+                    {/each}
                 </div>
             {/each}
-        </div>
-    {/if}
-</div>
-
+        {:else}
+            <div class="flex flex-col mx-4 mb-5 mt-2">
+                {#each tracks as track}
+                    <TrackWrapper track={track} tracks={tracks}>
+                        <div class="flex flex-row items-center hover:bg-secondary py-2 px-2 rounded-sm">
+                            {#await getImageUrl(track.image) then image}
+                                <Lazy keep={true}>
+                                    <img class="h-24 w-24 mr-4" src={image} alt={track.title} />
+                                </Lazy>
+                            {:catch error}
+                                <div class="h-24 w-24 bg-gray-500 mr-4"></div>
+                            {/await}
+                            <div class="flex flex-col items-start flex-grow">
+                                <h1 class="text-foreground text-lg font-bold leading-none mb-1">{track.title}</h1>
+                                <h1 class="text-slate-400 text-base font-light leading-none">{track.artist}</h1>
+                            </div>
+                            <div class="flex flex-col items-end text-right ml-4">
+                                <h1 class="text-slate-400 text-base font-light leading-none">{track.year}</h1>
+                                <h1 class="text-slate-400 text-base font-light leading-none">{track.artist}</h1>
+                            </div>
+                        </div>
+                    </TrackWrapper>
+                {/each}
+            </div>
+        {/if}
+    </div>
+{/if}
--- src/routes/albums/+page.svelte
+++ src/routes/albums/+page.svelte
@@ -3,7 +3,7 @@
     import { OPFS } from "$lib/opfs";
     import { onMount } from "svelte";
     import type { Album } from "$lib/types/album";
-    import { ArrowUpAZ, ArrowDownZA, ListFilter } from "lucide-svelte";
+    import { ArrowUpAZ, ArrowDownZA, ListFilter, List } from "lucide-svelte";
     import Button from "$lib/components/ui/button/button.svelte";
     import * as DropdownMenu from "$lib/components/ui/dropdown-menu/index.js";
     import { title } from "$lib/store";
@@ -27,6 +27,7 @@
 
     let sort = "title";
     let ascending = true;
+    let listType = "grid";
 
     async function sortAlbums(s: string) {
         sort = s;
@@ -42,6 +43,21 @@
     function swapAscending() {
         ascending = !ascending;
         sortAlbums(sort);
+    }
+
+    function formatDuration(duration: number): string {
+      const roundedDuration = Math.round(duration);
+      const minutes = Math.floor(roundedDuration / 60);
+      const seconds = roundedDuration % 60;
+      return `${minutes}:${seconds.toString().padStart(2, '0')}`;
+    }
+
+    function swapListType() {
+        if (listType === "list") {
+            listType = "grid";
+        } else {
+            listType = "list";
+        }
     }
 </script>
 
@@ -71,26 +87,55 @@
                 </DropdownMenu.RadioGroup>
             </DropdownMenu.Content>
         </DropdownMenu.Root>
+        <Button class="my-1 ml-3 h-10 w-10 bg-transparent px-1 hover:bg-secondary" on:click={() => swapListType()}>
+            <List size={20} color="white" />
+        </Button>
 </div>
 
-<div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-x-4 gap-y-6 sm:gap-x-6 md:gap-x-8 lg:gap-x-10 xl:gap-x-12 ml-16 my-5">
-    {#each albums as album}
-        <div class="flex flex-col items-start">
-            {#await getImageUrl(album.image) then image}
-                <a class="pointer" href={`/album?album=${album.id}`}>
-                    <Lazy keep={true}>
-                        <img class="h-52 w-52 rounded-sm" src={image} alt={album.name} />
-                    </Lazy>
-                </a>
-            <div class="flex flex-row items-start">
-                <div class="flex flex-col items-start h-full mt-4">
-                    <h1 class="text-foreground text-lg font-bold leading-none mb-1">{album.name}</h1>
-                    <h1 class="text-slate-400 text-base font-light leading-none p">{album.artist}</h1>
+{#if listType !== 'list'}
+    <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-x-4 gap-y-6 sm:gap-x-6 md:gap-x-8 lg:gap-x-10 xl:gap-x-12 ml-16 my-5">
+        {#each albums as album}
+            <div class="flex flex-col items-start">
+                {#await getImageUrl(album.image) then image}
+                    <a class="pointer" href={`/album?album=${album.id}`}>
+                        <Lazy keep={true}>
+                            <img class="h-52 w-52 rounded-sm" src={image} alt={album.name} />
+                        </Lazy>
+                    </a>
+                <div class="flex flex-row items-start">
+                    <div class="flex flex-col items-start h-full mt-4">
+                        <h1 class="text-foreground text-lg font-bold leading-none mb-1">{album.name}</h1>
+                        <h1 class="text-slate-400 text-base font-light leading-none p">{album.artist}</h1>
+                    </div>
                 </div>
+                {:catch error}
+                <div class="h-52 w-52 bg-gray-500 rounded-sm animate-pulse"></div>
+                {/await}
             </div>
-            {:catch error}
-            <div class="h-52 w-52 bg-gray-500 rounded-sm animate-pulse"></div>
-            {/await}
-        </div>
-    {/each}
+        {/each}
     </div>
+{:else}
+    <div class="flex flex-col mx-4 mb-5 mt-2" >
+        {#each albums as album}
+        <a class="pointer" href={`/album?album=${album.id}`}>
+        <div class="flex flex-row items-center hover:bg-secondary py-2 px-2 rounded-sm">
+            {#await getImageUrl(album.image) then image}
+            <Lazy keep={true}>
+                <img class="h-24 w-24 mr-4" src={image} alt={album.name} />
+            </Lazy>
+            {:catch error}
+                <div class="h-24 w-24 bg-gray-500 mr-4"></div>
+            {/await}
+            <div class="flex flex-col items-start flex-grow">
+                <h1 class="text-foreground text-lg font-bold leading-none mb-1">{album.name}</h1>
+                <h1 class="text-slate-400 text-base font-light leading-none">{album.artist}</h1>
+            </div>
+            <div class="flex flex-col items-end text-right ml-4">
+                <h1 class="text-slate-400 text-base font-light leading-none">{album.year}</h1>
+                <h1 class="text-slate-400 text-base font-light leading-none">{album.artist}</h1>
+            </div>
+        </div>
+        </a>
+        {/each}
+    </div>
+{/if}
