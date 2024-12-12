6a7cf4382371a0194b57cd64f14e32bcc8d1b7bc
Cattn <Cattn@mathstudy.dev>
2024-12-11 21:11:09 -0500

    PWA Fixes

--- src/service-worker/index.js
+++ src/service-worker/index.js
@@ -1,75 +1,64 @@
-// Import necessary modules and functions from workbox and service-worker
-// Note some of the libraries are inbuilt in Svelte so install only the missing ones
-import { build, files, prerendered, version } from "$service-worker";
-import { cacheNames, clientsClaim } from "workbox-core";
-import { cleanupOutdatedCaches, precacheAndRoute } from "workbox-precaching";
-import { NavigationRoute, registerRoute } from "workbox-routing";
-import * as navigationPreload from "workbox-navigation-preload";
-import { NetworkFirst, StaleWhileRevalidate } from "workbox-strategies";
-import { CacheableResponsePlugin } from "workbox-cacheable-response";
-import { ExpirationPlugin } from "workbox-expiration";
-import * as googleAnalytics from "workbox-google-analytics";
+/// <reference types="@sveltejs/kit" />
 
-// Ensure that the current service worker takes control of the page as soon as possible
-clientsClaim();
+// @ts-nocheck
+import { build, files, version } from '$service-worker';
 
-// Initialize Google Analytics tracking for your service worker (though it is to be depreciated)
-// This helps queue tracking request when offline and forward them when the network is back
-// if your not using analytics; you can remove this line and its import.
-googleAnalytics.initialize();
+// Create a unique cache name for this deployment
+const CACHE = `cache-${version}`;
 
-// Enable navigation preloading to improve the performance of navigation requests
-// We will be using this instead of Svelte's hence the conversion of links to reload instead of preload
-navigationPreload.enable();
+const ASSETS = [
+    ...build, // the app itself
+    ...files  // everything in `static`
+];
 
-// Define cache names for navigation and dynamic content
-// This also help with cache versioning
-const navigationCacheName = `${cacheNames.prefix}-navigations-${cacheNames.suffix}`;
-const dynamicContentCacheName = `${cacheNames.prefix}-dynamic-content-${cacheNames.suffix}`;
+self.addEventListener('install', (event) => {
+    // Create a new cache and add all files to it
+    async function addFilesToCache() {
+        const cache = await caches.open(CACHE);
+        await cache.addAll(ASSETS);
+    }
 
-// Create a new navigation route with a NetworkFirst strategy, which will try the network first and fall back to the cache if needed
-// This will handle all the routes except the dynamic ones
-const navigationRoute = new NavigationRoute(new NetworkFirst({
-cacheName: navigationCacheName,
-plugins: [
-new CacheableResponsePlugin({ statuses: [0, 200] }), // Only cache responses with a status of 0 or 200
-new ExpirationPlugin({
-maxAgeSeconds: 24 * 60 * 60 * 7, // Cache entries will be deleted after 7 days
-purgeOnQuotaError: true // If the cache is full, delete the oldest entries to make space for new ones
-})
-]
-}));
-
-// Register the navigation route
-registerRoute(navigationRoute);
-
-// Create a new StaleWhileRevalidate strategy for dynamic content, which will serve cached responses immediately and update them in the background
-const dynamicContentStrategy = new StaleWhileRevalidate({
-cacheName: dynamicContentCacheName,
-plugins: [
-new CacheableResponsePlugin({ statuses: [0, 200] }), // Only cache responses with a status of 0 or 200
-new ExpirationPlugin({
-maxAgeSeconds: 24 * 60 * 60 * 7, // Cache entries will be deleted after 7 days
-purgeOnQuotaError: true // If the cache is full, delete the oldest entries to make space for new ones
-})
-]
+    event.waitUntil(addFilesToCache());
 });
 
-// Register the dynamic content strategy for any URL that starts with "/post"
-// For example in this case the url is "https://www.example.com/post?id=681d6afa-2fab-401c-a8ea-22bd3a496723"
-registerRoute(
-({ url }) => url.pathname.startsWith("/post"),
-dynamicContentStrategy
-);
+self.addEventListener('activate', (event) => {
+    // Remove previous cached data from disk
+    async function deleteOldCaches() {
+        for (const key of await caches.keys()) {
+            if (key !== CACHE) await caches.delete(key);
+        }
+    }
 
-// Create a list of URLs to precache based on the build, files, and prerendered arrays from the service-worker module
-const precacheList = [...build, ...files, ...prerendered].map((s) => ({
-url: s,
-revision: version
-}));
+    event.waitUntil(deleteOldCaches());
+});
 
-// Precache and route the URLs in the precacheList
-precacheAndRoute(precacheList);
+self.addEventListener('fetch', (event) => {
+    // ignore POST requests etc
+    if (event.request.method !== 'GET') return;
 
-// Clean up outdated caches that are no longer needed
-cleanupOutdatedCaches();
+    async function respond() {
+        const url = new URL(event.request.url);
+        const cache = await caches.open(CACHE);
+
+        // `build`/`files` can always be served from the cache
+        if (ASSETS.includes(url.pathname)) {
+            return cache.match(url.pathname);
+        }
+
+        // for everything else, try the network first, but
+        // fall back to the cache if we're offline
+        try {
+            const response = await fetch(event.request);
+
+            if (response.status === 200) {
+                cache.put(event.request, response.clone());
+            }
+
+            return response;
+        } catch {
+            return cache.match(event.request);
+        }
+    }
+
+    event.respondWith(respond());
+});
