d781d5c41167df7dda8b1f2601d7159446f6880f
Logan <thecrazykid2121@gmail.com>
2025-01-13 16:51:27 -0500

    https/cert

--- api/maple.js
+++ api/maple.js
@@ -4,10 +4,24 @@ const login = require('./auth/login');
 const app = express();
 const PORT = 3000;
 
+const fs = require('fs');
+const https = require('https');
+
+const privateKey = fs.readFileSync('/etc/letsencrypt/live/maple.kolf.dev/privkey.pem', 'utf8');
+const certificate = fs.readFileSync('/etc/letsencrypt/live/maple.kolf.dev/cert.pem', 'utf8');
+const ca = fs.readFileSync('/etc/letsencrypt/live/maple.kolf.dev/chain.pem', 'utf8');
+
+const credentials = {
+	key: privateKey,
+	cert: certificate,
+	ca: ca
+};
+
 app.use(cors());
 app.use('/login', login);
 
-app.listen(PORT, () => {
-    console.log(`Listening on port ${PORT}`);
-});
+const httpsServer = https.createServer(credentials, app);
 
+httpsServer.listen(PORT, () => {
+	console.log('HTTPS Server running on port 443');
+});
