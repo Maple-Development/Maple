b3966c7fcd1d0b57bc0bb643779d11fd07b93909
Logan <thecrazykid2121@gmail.com>
2025-01-27 16:37:00 -0500

    extra give up

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,8 @@
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 //import adapter from '@sveltejs/adapter-auto';
-import adapter from '@sveltejs/adapter-node';
+//import adapter from '@sveltejs/adapter-node';
+import adapter from '@sveltejs/adapter-cloudflare';
+
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
