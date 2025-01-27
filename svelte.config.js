748d18e6003fec3c90ea1d4afaca41cb48b24f94
Logan <thecrazykid2121@gmail.com>
2025-01-27 14:08:10 -0500

    adapter auto

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,6 @@
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-//import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-auto';
 //import adapter from '@sveltejs/adapter-static';
-import adapter from '@sveltejs/adapter-cloudflare';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
