976a8a60fa3eb2fd55faf58925364ae853d886ec
Logan <thecrazykid2121@gmail.com>
2025-04-11 23:41:32 -0400

    im dumb

--- src/lib/preferences/usersettings.ts
+++ src/lib/preferences/usersettings.ts
@@ -7,18 +7,21 @@ let webhookSettings = new Settings('webhook');
 webhookEnabled = webhookSettings.get('enabled') !== null ? webhookSettings.get('enabled') : webhookEnabled;
 webhookURL = webhookSettings.get('url') !== null ? webhookSettings.get('url') : webhookURL;
 
+let discord = false;
+
+let discordSettings = new Settings('discord');
+discord = discordSettings.get('enabled') !== null ? discordSettings.get('enabled') : discord;
+
 let p2p = true;
 let devMode = false;
 let showLogging = false;
 let socket = true;
-let discord = false;
 
 let preferences = new Settings('preferences');
 p2p = preferences.get('p2p') !== null ? preferences.get('p2p') : p2p;
 devMode = preferences.get('devMode') !== null ? preferences.get('devMode') : devMode;
 showLogging = preferences.get('showLogging') !== null ? preferences.get('showLogging') : showLogging;
 socket = preferences.get('socket') !== null ? preferences.get('socket') : socket;
-discord = preferences.get('discord') !== null ? preferences.get('discord') : discord;
 
 const UserSettings = {
 	webhook: {
