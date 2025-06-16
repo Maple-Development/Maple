f97cac3e28d601aae9cd8557828e6be28a834766
Logan <thecrazykid2121@gmail.com>
2025-06-15 23:01:35 -0400

    built-in media serve

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 import { handler } from './build/handler.js';
 import express from 'express';
-
-const app = express();
-
+import path from 'path';
 import * as fs from 'fs';
 import https from 'node:https';
+
+const app = express();
 
 const privateKey = fs.readFileSync('/etc/letsencrypt/live/maple.kolf.pro/privkey.pem', 'utf8');
 const certificate = fs.readFileSync('/etc/letsencrypt/live/maple.kolf.pro/cert.pem', 'utf8');
@@ -17,6 +17,11 @@ const credentials = {
 };
 
 const server = https.createServer(credentials, app);
+
+const uploadsPath = path.join(process.cwd(), 'SMS', 'uploads');
+if (fs.existsSync(uploadsPath)) {
+    app.use('/SMS/uploads', express.static(uploadsPath));
+}
 
 // let SvelteKit handle everything else, including serving prerendered pages and static assets
 app.use(handler);
