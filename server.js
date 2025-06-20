d301f3f112fa3af15d805f06e5d212045d0fe03a
Logan <Logan@cattn.dev>
2025-06-20 03:52:52 -0400

    http server

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ import { handler } from './build/handler.js';
 import express from 'express';
 import path from 'path';
 import * as fs from 'fs';
-import https from 'node:https';
 import http from 'node:http';
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
@@ -11,17 +10,7 @@ const __dirname = dirname(__filename);
 
 const app = express();
 
-const privateKey = fs.readFileSync('/etc/letsencrypt/live/maple.kolf.pro/privkey.pem', 'utf8');
-const certificate = fs.readFileSync('/etc/letsencrypt/live/maple.kolf.pro/cert.pem', 'utf8');
-const ca = fs.readFileSync('/etc/letsencrypt/live/maple.kolf.pro/chain.pem', 'utf8');
-
-const credentials = {
-    key: privateKey,
-    cert: certificate,
-    ca: ca
-};
-
-const server = https.createServer(credentials, app);
+const server = http.createServer(app);
 
 const uploadsPath = path.join(__dirname, 'SMS', 'uploads');
 if (fs.existsSync(uploadsPath)) {
