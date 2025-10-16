f3217ed3d957022642e9fcb2fe9df97e8a772395
Logan <thecrazykid2121@gmail.com>
2025-10-16 14:55:39 -0400

    fix logins

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,11 +1,21 @@
-<form method="POST">
-	<label>
-		Username
-		<input name="username" type="text">
-	</label>
-	<label>
-		Password
-		<input name="password" type="password">
-	</label>
-	<button>Log in</button>
+<script lang="ts">
+    import { UserManager } from '$lib/api/UserManager';
+    let username = '';
+    let password = '';
+    async function onSubmit(event: SubmitEvent) {
+        event.preventDefault();
+        await UserManager.login(username, password);
+    }
+</script>
+
+<form on:submit|preventDefault={onSubmit}>
+    <label>
+        Username
+        <input name="username" type="text" bind:value={username}>
+    </label>
+    <label>
+        Password
+        <input name="password" type="password" bind:value={password}>
+    </label>
+    <button>Log in</button>
 </form>
